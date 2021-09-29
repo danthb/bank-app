78f86049b66090be415f3d288c2f446fec3be81c
danthb <danthb@hotmail.com>
2021-09-29 17:16:40 -0500

    Updates

---
+++ static/js/7.a8aadd66.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([7],{46:function(e,t,a){"use strict";function n(){return c.a.createElement(o.a,{bgcolor:"black",txtcolor:"white",header:"Badbank Landing Page",title:"Welcome to the Bank",text:"You can use this Bank",body:c.a.createElement("img",{src:"bank.png",className:"img-fluid",alt:"Responsive",fill:"red"})})}Object.defineProperty(t,"__esModule",{value:!0}),t.default=n;var l=a(0),c=a.n(l),o=a(17)}});
+//# sourceMappingURL=7.a8aadd66.chunk.js.map
